82fd33f5c04d0cffbb85a1399d800b21cb05df52
Felipe França <felipe.franca.108@ufrn.edu.br>
2022-04-28 08:13:51 -0300

    correcting foreignkey object data return

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -1,12 +1,16 @@
 from rest_framework import serializers
 from ..models import Profile, CustomUser as User
 
-class ProfileSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = Profile
-        fields = '__all__'
 
 class UserSerializer(serializers.ModelSerializer):
+
     class Meta:
         model = User
+        fields = '__all__'
+
+class ProfileSerializer(serializers.ModelSerializer):
+    related_user = UserSerializer()
+
+    class Meta:
+        model = Profile
         fields = '__all__'
