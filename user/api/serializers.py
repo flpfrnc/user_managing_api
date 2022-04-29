93c6d48d2d29d52f7fbddf4653f829afda25560b
Felipe França <felipe.franca.108@ufrn.edu.br>
2022-04-29 10:15:01 -0300

    correcting update foreign key on id

--- user/api/serializers.py
+++ user/api/serializers.py
@@ -9,8 +9,12 @@ class UserSerializer(serializers.ModelSerializer):
         fields = '__all__'
 
 class ProfileSerializer(serializers.ModelSerializer):
-    related_user = UserSerializer()
 
     class Meta:
         model = Profile
         fields = '__all__'
+
+    def to_representation(self, instance):
+       ret = super().to_representation(instance)
+       ret['related_user'] = UserSerializer(instance.related_user).data
+       return ret
